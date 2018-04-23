8d63e9260022ca9ab7cee647de2c9a84244e964e
problemofpoints <problemofpoints@gmail.com>
2018-04-23 18:06:15 -0500

    initial commit

---
+++ R/calculate-buyback-roi.R
@@ -0,0 +1,67 @@
+
+# do buyback calculations and create exhibits
+
+
+# setup -----------------------------------------------------------
+
+library(knitr)
+library(AonInternal)
+library(tidyverse)
+library(tibbletime)
+library(lubridate)
+library(tidyquant)
+library(flextable)
+library(AonECM)
+library(officer)
+library(patchwork)
+
+AonInternal::SetTheme()
+aon_colors <- c("#0083A9", "#808080", "#BFBFBF", "#5EB6E4", "#F0AB00", "#7AB800", 
+                "#6E267B", "#BC5FCD","#E9CAEE", "#D3CD8B", "#7E7830", "#3E5B00", "#E11B22")
+
+AonECM::ggSetTheme()
+
+
+# import data -------------------------------------------------------------
+
+data_to_use <- readRDS("data/buyback-roi-data.rds")
+
+
+# buyback roi calculations ------------------------------------------------
+
+data_to_use <- data_to_use %>%
+  mutate(cum_shares_repurchased = cumsum(sharesrepurchased)) %>%
+  mutate(cash_flow = -dollarrepurchase + (commondivpershare + specdivpershare) * cum_shares_repurchased,
+         cash_flow_final = cash_flow + if_else(year == 2017 & qtr_ == 4, close * cum_shares_repurchased, 0),
+         cash_flow_final2 = cash_flow + if_else(year == 2017 & qtr_ == 4, adjusted * cum_shares_repurchased, 0))
+
+buyback_roi <- data_to_use %>% 
+  summarise(n_qtrs = n(),
+            market_cap = market_cap[1], init_shares = commonshares[1], 
+            shares_repurchased = dplyr::last(cum_shares_repurchased), 
+            dollar_repurchased = sum(dollarrepurchase), 
+            pct_init_shares = shares_repurchased / init_shares, 
+            pct_market_cap = sum(dollarrepurchase) / market_cap[1],
+            tsr = ((sum((commondivpershare + specdivpershare) * commonshares) / dplyr::last(commonshares) + 
+                      dplyr::last(close)) / dplyr::first(close))^(1/(n_qtrs/4)) - 1,
+            irr = (1+FinCal::irr(cash_flow_final))^4 - 1,
+            buyback_effectiveness = (1+irr)/(1+tsr) - 1) %>%
+  filter(pct_market_cap > 0.01) %>%
+  arrange(desc(buyback_effectiveness))
+
+buyback_roi_output <- buyback_roi %>%
+  transmute(Ticker = ticker,
+            `# of quarters` = n_qtrs,
+            `Current Mkt Cap ($m)` = number_format(market_cap / 1e6, 0),
+            `Initial Shares (mil)` = number_format(init_shares / 1e6, 1),
+            `Shares Repurchased (mil)` = number_format(shares_repurchased / 1e6,1),
+            `Dollar Repurchased ($m)` = number_format(dollar_repurchased / 1e6,0),
+            `Percent of Initial Shares Repurchased` = pct_format(pct_init_shares, 1),
+            `Percent of Mkt Cap Repurchased` = pct_format(pct_market_cap,1),
+            `Total Shareholder Return` = pct_format(tsr, 1),
+            `Buyback ROI` = pct_format(irr, 1),
+            `Buyback Effectiveness` = pct_format(buyback_effectiveness,1))
+
+# create exhibits ---------------------------------------------------------
+
+
---
+++ R/get-buyback-roi-data.R
@@ -0,0 +1,71 @@
+
+# get data needed for buyback roi analysis
+
+library(tidyverse)
+library(readxl)
+library(tibbletime)
+library(lubridate)
+library(tidyquant)
+
+
+# input variables ---------------------------------------------------------
+
+from_date <- as.Date("2013-01-01")
+to_date <- as.Date("2017-12-31")
+
+# import buyback data from spreadsheet ------------------------------------
+
+data <- readxl::read_xlsx("data-raw/insurer_buybacks.xlsx", sheet = "import") %>%
+  gather(key = "statistic", value = "value", -ticker, -company) %>%
+  separate(statistic, c("yr_qtr","metric"), sep = "_") %>%
+  separate(yr_qtr, c("year","qtr"), sep = 4, remove = FALSE) %>%
+  mutate(value = as.double(value), 
+         year = as.double(year),
+         date = lubridate::yq(yr_qtr), 
+         qtr_ = lubridate::quarter(date)) %>%
+  replace_na(list(value = 0)) %>% 
+  spread(key = metric, value = value) %>%
+  mutate(dollarrepurchase = avgpricepershare * sharesrepurchased)
+
+# import stock price data -------------------------------------------------
+
+# get a list of all the Property-Casualty Insurers from the major US stock exchanges
+stock_tickers <- list('AMEX','NASDAQ','NYSE') %>% map_df(tq_exchange) %>% 
+  filter((industry == 'Property-Casualty Insurers' | symbol %in% c("AGII", "ERIE", "AIZ")) & !is.na(market.cap)) %>%
+  mutate(market_cap = if_else(stringr::str_sub(market.cap,start = -1)=="M",1e6,1e9) *
+           as.numeric(stringr::str_sub(market.cap,start = 2, end = -2))) %>%
+  dplyr::select(symbol, company, sector, industry, market_cap) %>%
+  arrange(desc(market_cap))
+
+stock_tickers <- stock_tickers %>% 
+  inner_join(data %>% distinct(ticker), by = c("symbol"="ticker"))
+
+# now get the historical stock prices for all of the tickers; get quarter close stock prices
+stock_prices <- stock_tickers %>%
+  tq_get(get = "stock.prices", from = from_date, to = to_date) %>% 
+  filter(!is.na(adjusted)) %>% 
+  select(symbol, company, market_cap, date, close, adjusted) 
+
+stock_prices_qtr <- stock_prices %>%
+  group_by(symbol) %>%
+  as_tbl_time(index = date) %>%
+  as_period("quarterly", side = "end") %>%
+  mutate(year = lubridate::year(date), qtr_ = lubridate::quarter(date))
+
+
+# combine input data ------------------------------------------------------
+
+data_to_use <- data %>% 
+  select(-date) %>% 
+  left_join(stock_prices_qtr %>% 
+              select(symbol, market_cap, year, date, qtr_, close, adjusted), 
+            by = c("ticker"="symbol", "year", "qtr_")) %>%
+  drop_na() %>%
+  group_by(ticker) %>%
+  as_tbl_time(index = date) %>%
+  filter_time(as.character(lubridate::year(from_date)) ~ as.character(lubridate::year(to_date)))
+
+# save data -------------------------------------------------------------
+
+saveRDS(data_to_use, "data/buyback-roi-data.rds")
+
